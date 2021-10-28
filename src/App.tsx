453d2c37274441db69b95088e6b85cce9813da2a
GlennMiller1991 <gatesoftommorow91@mail.ru>
2021-10-28 12:24:56 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+    //initial state
+    const [seconds, setSeconds] = useState(() => 77726)
+
+    //dinamic styles
+    const secondsStyle = {
+        transform: `rotate(${seconds * 6}deg`
+    }
+    const minutesStyle = {
+        transform: `rotate(${seconds * 0.1}deg`
+    }
+    const hoursStyle = {
+        transform: `rotate(${seconds * 0.0083333333}deg`
+    }
+
+    useEffect(() => {
+        setInterval(() => {
+            setSeconds(seconds => seconds + 1)
+        }, 1000)
+    }, [])
+    console.log(new Date().getSeconds())
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="clock">
+        <div className="outer-clock-face">
+          <div className="marking marking-one"></div>
+          <div className="marking marking-two"></div>
+          <div className="marking marking-three"></div>
+          <div className="marking marking-four"></div>
+          <div className="inner-clock-face">
+              <span>The Clock<br/>does not reflect<br/>the current time</span>
+            <div className="hand hour-hand" style={hoursStyle}></div>
+            <div className="hand min-hand" style={minutesStyle}></div>
+            <div className="hand second-hand" style={secondsStyle}></div>
+          </div>
+        </div>
+      </div>
+  )
 }
 
 export default App;
